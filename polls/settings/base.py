2288953d0d22a973e89e1bb86dc85cc55e7796fe
Naman Chauhan <chauhannaman98@gmail.com>
2020-07-22 14:46:08 +0530

    update: remove redundant config

--- polls/settings/base.py
+++ polls/settings/base.py
@@ -22,14 +22,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '2tt#2!dy6t)i5^c2k%tg&1vo3vjgji#qn@1t%&a#*i!hppx@)*'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = [
-    'pollsapp-django.herokuapp.com',
-    'pollsapp-staging.herokuapp.com'
-    ]
-
 
 # Application definition
 
